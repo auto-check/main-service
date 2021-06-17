8120348906739d61b2a5a1c6d14246332e4bfe9c
hwc9169 <hwc9169@gmail.com>
2021-06-17 23:20:10 +0900

    Update move model to diff repo

--- model/macro.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Macro struct {
-	ID int64 `json:"id"`
-	StudentID int64 `json:"student_id"`
-	Birth string `json:"birth"`
-	Password string `json:"password"`
-	OnOff bool `json:"onoff"`
-}
