b631e4c8198468d06bebfa7125895b6c18c767cd
hwc9169 <hwc9169@gmail.com>
2021-06-21 22:32:54 +0900

    Add main service client

---
+++ client/main_client.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	mainpb "github.com/auto-check/main-service/protocol-buffer/golang/main"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"os"
+	"sync"
+)
+
+func init() {
+	err := 	godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var (
+	once sync.Once
+	cli mainpb.MainClient
+)
+
+func GetMainClient() mainpb.MainClient {
+	once.Do(func() {
+		conn, _ := grpc.Dial(
+			"127.0.0.1:"+os.Getenv("PORT"),
+			grpc.WithInsecure(),
+			grpc.WithBlock())
+
+		cli = mainpb.NewMainClient(conn)
+	})
+
+	return cli
+}
