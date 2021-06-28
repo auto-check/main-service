7a5034358b75a13c62830cd1f3e5221b542e5eef
hwc9169 <hwc9169@gmail.com>
2021-06-28 22:50:42 +0900

    update NewMacroHandler

--- handler/macro_handler.go
+++ handler/macro_handler.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"context"
 	"github.com/auto-check/main-service/usecase"
+	authpb "github.com/auto-check/protocol-buffer/golang/auth"
 	mainpb "github.com/auto-check/protocol-buffer/golang/main"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -11,11 +13,13 @@ import (
 type MacroHandler struct {
 	mainpb.MainServer
 	su usecase.MacroUsecase
+	ac authpb.AuthClient
 }
 
-func NewMacroHandler(gserver *grpc.Server, us usecase.MacroUsecase) *MacroHandler {
+func NewMacroHandler(gserver *grpc.Server, us usecase.MacroUsecase, ac authpb.AuthClient) *MacroHandler {
 	handler := &MacroHandler{
 		su: us,
+		ac: ac,
 	}
 	mainpb.RegisterMainServer(gserver, handler)
 	return handler
@@ -29,5 +33,10 @@ func (sh *MacroHandler) CreateMacro(ctx context.Context, r *emptypb.Empty) (*emp
 		return &emptypb.Empty{}, err
 	}
 	return &emptypb.Empty{}, nil
+}
+
+func (sh *MacroHandler) GetMacroStatus(ctx context.Context, r *emptypb.Empty) (*mainpb.GetMacroStatusResponse, error) {
+	id, _ := ctx.Value("student_id").(int64)
+	log.Println(id)
 }
 
