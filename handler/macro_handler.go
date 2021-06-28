69afd263654160a2e68b4ed20a738771ad3b5300
hwc9169 <hwc9169@gmail.com>
2021-06-28 22:14:10 +0900

    get id from context

--- handler/macro_handler.go
+++ handler/macro_handler.go
@@ -2,15 +2,10 @@ package handler
 
 import (
 	"context"
-	"fmt"
 	"github.com/auto-check/main-service/usecase"
 	mainpb "github.com/auto-check/protocol-buffer/golang/main"
-	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"runtime/debug"
-	"strconv"
 )
 
 type MacroHandler struct {
@@ -27,15 +22,9 @@ func NewMacroHandler(gserver *grpc.Server, us usecase.MacroUsecase) *MacroHandle
 }
 
 func (sh *MacroHandler) CreateMacro(ctx context.Context, r *emptypb.Empty) (*emptypb.Empty, error) {
-	md, _ := metadata.FromIncomingContext(ctx)
-	log.Println(ctx.Value("student_id"))
-	id, err := strconv.Atoi(md.Get("student_id")[0])
-	if err != nil {
-		log.Errorf(fmt.Sprintf("Error %s\n%s", err, debug.Stack()))
-		return &emptypb.Empty{}, err
-	}
+	id, _ := ctx.Value("student_id").(int64)
 
-	err = sh.su.CreateMacroByStudentID(int64(id))
+	err := sh.su.CreateMacroByStudentID(int64(id))
 	if err != nil {
 		return &emptypb.Empty{}, err
 	}
